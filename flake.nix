e772003b0e04173f468a46e7b6fc623055ddc4d4
Elena Nardi <elena.nardi@lip6.fr>
2025-11-25 15:51:05 +0100

    release(project): 0.15.4 → 0.15.5

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         packages = {
           caracal = pkgs.stdenv.mkDerivation {
             pname = "caracal";
-            version = "0.15.4";
+            version = "0.15.5";
             src = self;
             nativeBuildInputs = [
               pkgs.cmake
