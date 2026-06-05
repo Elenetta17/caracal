f2d217e6f1d71a0a7db7a83c9a3a24a8babcdfa9
Elena Nardi <elena.nardi@lip6.fr>
2026-06-05 14:15:19 +0200

    fix: pass capture_timestamp as-is without microsecond conversion

--- src/reply.cpp
+++ src/reply.cpp
@@ -23,7 +23,7 @@ std::string Reply::to_csv(const std::string& round) const {
                  std::back_inserter(mpls_labels_csv), mpls_label_to_csv);
   return fmt::format(
       "{},{},{},{},{},{},{},{},{},{},{},{},{},{},\"[{}]\",{},{}",
-      capture_timestamp / 1'000'000, probe_protocol, reply_dst_addr,
+      capture_timestamp, probe_protocol, reply_dst_addr,
       probe_dst_addr, probe_src_port, probe_dst_port, probe_ttl, quoted_ttl,
       reply_src_addr, reply_protocol, reply_icmp_type, reply_icmp_code,
       reply_ttl, reply_size, fmt::join(mpls_labels_csv, ","), rtt, round);
